0d202710148be06fa3621b022da7712161311a4d
Muhammad Hassaan Naeem <muhammadhassaannaeem426@gmail.com>
2026-04-22 22:10:55 +0500

    feat: add research package init
    Added module documentation for thesis research.

--- src/aquasense/research/__init__.py
+++ src/aquasense/research/__init__.py
@@ -1,1 +1,13 @@
+"""
+research/
+---------
+Thesis research modules for:
+"Energy-Efficient Depth-Aware Clustering-Based Routing Protocol
+ for Internet of Underwater Things (IoUT) Using Depth Sensors"
 
+Author : Muhammad Hassaan Naeem
+Modules:
+    routing_protocol  – Proposed CH selection + multi-hop routing
+    energy_model      – Acoustic energy consumption model
+    benchmarks        – Protocol comparison framework
+"""
